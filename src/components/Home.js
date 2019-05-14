08e9eba31af04fd201d87448ac8225345ba4cdf8
Andre Hadianto <andre_hadianto@mymail.sutd.edu.sg>
2019-05-14 18:26:54 +0800

    setting up containers for homepage

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,10 +1,22 @@
 import React from 'react';
+import '../css/reuseable.css';
+import '../css/home.css';
 
 class Home extends React.Component {
     render() {
         return (
-            <div>
-                Hello world!
+            <div className="home-container">
+                <div className="profile-wrapper">
+                    <div className="home-nav">
+                        navigation
+                    </div>
+                    <div className="home-content">
+                        my namee
+                    </div>
+                </div>
+                <div className="hero-wrapper">
+                    profile picture on the right side
+                </div>
             </div>
         )
     }
