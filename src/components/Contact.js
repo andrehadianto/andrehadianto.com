df4259ba3865dee8536349de3cdb4aec54090542
Andre Hadianto <andre_hadianto@mymail.sutd.edu.sg>
2019-05-17 11:58:39 +0800

    setting css file, creating nav bar, contacts, and hero image

---
+++ src/components/Contact.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+import '../css/reuseable.css';
+
+class Contacts extends React.Component {
+    render() {
+        return (
+            <div>
+
+            </div>
+        );
+    }
+}
+
+export default Contacts;
--- src/components/Home.js
+++ src/components/Home.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Nav, Navbar } from 'react-bootstrap';
 import '../css/reuseable.css';
 import '../css/home.css';
 
@@ -8,10 +9,18 @@ class Home extends React.Component {
             <div className="home-container">
                 <div className="profile-wrapper">
                     <div className="home-nav">
-                        navigation
+                        <Navbar expand="md">
+                            <Nav className="mr-auto">
+                                <Nav.Link>About Me</Nav.Link>
+                                <Nav.Link>Portfolio</Nav.Link>
+                                <Nav.Link>Photos</Nav.Link>
+                                <Nav.Link>Contact</Nav.Link>
+                            </Nav>
+                        </Navbar>
                     </div>
                     <div className="home-content">
-                        my namee
+                        <h1><span className="jumbo-name">Andre Hadianto Lesmana</span></h1>
+                        <p><span className="sub-jumbo-name">Finding inspiration :)</span></p>
                     </div>
                 </div>
                 <div className="hero-wrapper">
